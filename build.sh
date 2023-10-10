001a93c7e364341ac1c7560c5d4d67bc00ebe73a
Alex Woodcock <alex.woodcock@outlook.com>
2023-10-10 20:45:51 +1030

    minor

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
-./configure --cxx-common="-std=c++20 -Wno-misleading-indentation -Wno-deprecated-declarations"
+./configure --cxx-common="-std=c++20 --enable-debug -Wno-misleading-indentation -Wno-deprecated-declarations"
 make clean
-make
+make -j16
+
+
+# WARNING: pythia8-config doesn't actually work
+# g++ main01.cc -o main01 `../bin/pythia8-config --cppflags --libs`
+
+# just run example using
+# cd examples
+# g++ main01.cc -o main01 -L/mnt/UbuntuData2/pythiaGPU/lib -lpythia8 -I/mnt/UbuntuData2/pythiaGPU/include -std=c++20 -Wno-misleading-indentation -Wno-deprecated-declarations
+# ./main01 > main01.txt
