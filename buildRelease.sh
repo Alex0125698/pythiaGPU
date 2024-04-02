8e3274acf0bdbeef48a638b5bd08fddf7d971ce1
Alex Woodcock <alex.woodcock@outlook.com>
2024-04-02 15:22:45 +1030

    add build scripts

---
+++ buildRelease.sh
@@ -0,0 +1,17 @@
+clear
+./configure --enable-debug --cxx-common="-O2 -std=c++20 -Wno-misleading-indentation -Wno-deprecated-declarations"
+make clean
+make -j16
+
+
+# WARNING: pythia8-config doesn't actually work
+# g++ main01.cc -o main01 `../bin/pythia8-config --cppflags --libs`
+
+# just run example using
+cd examples
+g++ main01.cc -o main01 -O2 -L/mnt/UbuntuData2/pythiaDevelopment/lib -lpythia8 -I/mnt/UbuntuData2/pythiaDevelopment/include -std=c++20 -Wno-misleading-indentation -Wno-deprecated-declarations
+./main01 > main01.txt
+
+
+# g++ atest.cc -o atest -O2 -L/mnt/UbuntuData2/pythiaDevelopment/lib -lpythia8 -I/mnt/UbuntuData2/pythiaDevelopment/include -std=c++20 -Wno-misleading-indentation -Wno-deprecated-declarations
+# ./atest > atest.txt
